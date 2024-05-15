1badb72ae8ec17e0b0c9d39d0d50c0f615dff836
Jiani Fan <jiani.fan@gmail.com>
2024-05-15 14:59:33 -0700

    Tidy up for Heroku deploy

--- backend/haiku-backend/index.js
+++ backend/haiku-backend/index.js
@@ -1,23 +1,32 @@
-const express = require('express');
-const haikuController = require('./controllers/haikuController');
-const midnightJob = require('./utils/scheduler');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const haikuController = require("./controllers/haikuController");
+const midnightJob = require("./utils/scheduler");
+const cors = require("cors");
 
 const app = express();
-const backendPort = process.env.PORT || 3307; // Use the correct backend port
-const frontendPort = 3000; // Specify the frontend port explicitly
+const PORT = process.env.PORT || 3307; // Use the correct port for deployment
 
-const frontendOrigin = `http://localhost:${frontendPort}`; // Construct the frontend origin URL
+const frontendOrigin = process.env.FRONTEND_ORIGIN || "http://localhost:3000"; // Use environment variable for frontend origin
 
 app.use(cors({ origin: frontendOrigin })); // Allow requests from the frontend origin
 app.use(express.json());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../../frontend/build")));
+
 // Initialize routes
-app.use('/haikus', haikuController);
+app.use("/haikus", haikuController);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
+});
 
 // Use the scheduled job
 midnightJob;
 
-app.listen(backendPort, () => {
-  console.log(`Server is running on port ${backendPort} :)`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} :)`);
 });
